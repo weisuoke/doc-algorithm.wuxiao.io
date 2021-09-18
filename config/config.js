0f2867c3f0c8ec3b8163c1ef759338d73039621b
梅林的法杖 <530264127@qq.com>
2021-09-18 23:34:01 +0800

    init

---
+++ config/config.js
@@ -0,0 +1,21 @@
+const config = {
+  mode: 'site',
+  title: 'Algorithm Document',
+  description: '架构|思路|效率',
+  publicPath: '/',
+  base: '/',
+  exportStatic: {},
+  dynamicImport: {},
+  theme: {
+    '@primary-color': '#00A7D6',
+  },
+  navs: [
+    null
+  ]
+};
+
+if (process.env.NODE_ENV !== 'development') {
+  config.ssr = {};
+}
+
+export default config;
---
+++ docs/index.md
@@ -0,0 +1,11 @@
+---
+title: Algorithm Document
+order: 10
+hero:
+    title: Algorithm Document
+    desc: 📚 笔记 | 思路 | 扩展
+    actions:
+        - text: 开始阅读
+    link: /
+footer: Open-source MIT Licensed | Copyright © 2021-present<br />Powered by wuxiao.io
+---
